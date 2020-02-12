cfc4246a1d94b6a4f1a53bcc6b70eb1f85b61190
mantrabrain <mantrabrain@gmail.com>
2020-02-12 22:36:50 +0545

    update

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Mantrabrain
 Tags: demo import, tools, elementor template library
 Requires at least: 4.7
 Tested up to: 5.3.2
-Stable tag: 1.0.3
+Stable tag: 1.0.4
 License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
@@ -19,6 +19,9 @@ Extend Yatri theme feature with demo importer functionality & add Elementor widg
 
 
 == Changelog ==
+
+= 1.0.4 | 2020/02/12 =
+* Update elementor block
 
 = 1.0.3 | 2020/02/12 =
 * Update plugin add issue
--- yatri-tools.php
+++ yatri-tools.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Yatri Tools
  * Plugin URI:        https://wordpress.org/plugins/yatri-tools
  * Description:       Extend Yatri theme feature with demo importer functionality & add Elementor widgets, blocks, pages template library
- * Version:           1.0.3
+ * Version:           1.0.4
  * Author:            Mantrabrain
  * Author URI:        https://mantrabrain.com/
  * License:           GPL-2.0+
@@ -23,7 +23,7 @@ if (!defined('YATRI_TOOLS_FILE')) {
 
 // Define YATRI_TOOLS_VERSION.
 if (!defined('YATRI_TOOLS_VERSION')) {
-    define('YATRI_TOOLS_VERSION', '1.0.3');
+    define('YATRI_TOOLS_VERSION', '1.0.4');
 }
 
 // Define YATRI_TOOLS_PLUGIN_URI.
